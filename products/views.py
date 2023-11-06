ac70d429cbe5068ff1674bc3e8662e3a0bf044fe
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-11-06 05:51:53 +0400

    feat: template

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html')
+    return render(request, 'index.html', {"products": products})
 
 def new(request):
     return HttpResponse('New Products')
